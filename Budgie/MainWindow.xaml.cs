06eb90b0d406603e3120f8fc3417c409b263d817
rahoc <rahoc@aal-web.de>
2013-11-13 10:13:58 +0100

    1st Version Bachelorarbeit Tool
    Mainwindow and some random valuus in Personmodel

---
+++ Budgie/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using CommonLib;
+
+
+namespace Budgie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            while(true){
+                TestPerson p = new TestPerson("A");
+                Console.WriteLine(p.Person.Name.First());
+                Console.WriteLine(p.Person.Age.First());
+
+                Console.WriteLine("Geschlecht");
+                Console.WriteLine(p.Person.Gender[Gender.male]);
+                Console.WriteLine(p.Person.Gender[Gender.female]);
+
+                Console.WriteLine("IntegrationPattern");
+                Console.WriteLine(p.Person.IntegrationPattern[IntegrationPattern.sequential]);
+                Console.WriteLine(p.Person.IntegrationPattern[IntegrationPattern.simultaneous]);
+
+                Console.WriteLine("Handicaps");
+                Console.WriteLine(p.Person.HandicapHearing[true]);
+                Console.WriteLine(p.Person.HandicapHearing[false]);
+                Console.WriteLine(p.Person.HandicapMobility[true]);
+                Console.WriteLine(p.Person.HandicapMobility[false]);
+                Console.WriteLine(p.Person.HandicapSpeaking[true]);
+                Console.WriteLine(p.Person.HandicapSpeaking[false]);
+                Console.WriteLine(p.Person.HandicapVision[true]);
+                Console.WriteLine(p.Person.HandicapVision[false]);
+                Thread.Sleep(1000);
+            }
+
+            //XElement xe = XElement.Load(@"XML\PersonModel.xml");
+
+            //TextBox tbName = new TextBox();
+            //XElement xName = xe.Element("name");
+            //XElement xpString = xName.Element("pString");
+            //XAttribute attr = xpString.Attribute("value");
+            //tbName.Text = attr.Value;
+            //MainPanel.Children.Add(tbName);
+
+            //TextBox tbAge = new TextBox();
+            //tbAge.Text = xe.Element("age").Element("pInt").Attribute("value").Value;
+            //MainPanel.Children.Add(tbAge);
+
+
+            //foreach(XElement x in xe.Elements())
+            //{
+            //    TextBox tb = new TextBox();
+            //    tb.Name = x.Name.LocalName;
+            //    foreach(XElement d in x.Elements()) {
+            //        if (d.Attribute("value") != null)
+            //        {
+            //            tb.Text = tb.Text + d.Attribute("value").Value + "\n";
+            //        }
+            //        else
+            //        {
+            //            foreach (XElement d2 in d.Elements())
+            //            {
+            //                if (d2.Attribute("value") != null)
+            //                {
+            //                    tb.Text = tb.Text + d2.Attribute("value").Value + "\n";
+            //                }
+            //                else
+            //                {
+            //                    foreach (XElement d3 in d2.Elements())
+            //                    {
+            //                        if (d3.Attribute("value") != null)
+            //                        {
+            //                            tb.Text = tb.Text + d3.Attribute("value").Value + "\n";
+            //                        }
+            //                        else
+            //                        {
+            //                            tb.Text = tb.Text + "sth missing" + "\n";
+            //                        }
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //    MainPanel.Children.Add(tb);
+            //}
+        }
+    }
+}
---
+++ Budgie/TestPerson.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLib;
+using System.Security.Cryptography;
+
+namespace Budgie
+{
+    class TestPerson
+    {
+        PersonModel person;
+
+        public PersonModel Person
+        {
+            get { return person; }
+            set { person = value; }
+        }
+
+        public TestPerson(string id)
+        {
+            // Variablendeklaration
+            float random1;
+            float random2;
+
+            // Erzeuge neues PersonModel
+            this.Person = new PersonModel(id);
+            
+            // Name
+            DeutscheNamen names = new DeutscheNamen();
+            this.Person.Name.Add(names.Names[GenerateRandomNumber(0,names.Names.Length)], 1);
+
+            // Alter
+            this.Person.Age.Add(GenerateRandomNumber(1,99), 1);
+            
+            // Geschlecht
+            GenerateRandomDistribution(out random1, out random2);
+            // TODO: Muss/Sollte hier gerundet werden?
+            this.Person.Gender.Add(Gender.male, random1);
+            this.Person.Gender.Add(Gender.female, random2);
+
+            // Integration Pattern
+            GenerateRandomDistribution(out random1, out random2);
+            this.Person.IntegrationPattern.Add(IntegrationPattern.sequential, random1);
+            this.Person.IntegrationPattern.Add(IntegrationPattern.simultaneous, random2);
+
+            // Emotion
+            // TODO: Wie genau funktionert padValue? 1-10 Werte oder auch max zwei?
+
+            // Handicaps
+            GenerateRandomDistribution(out random1, out random2);
+            this.Person.HandicapHearing.Add(true, random1);
+            this.Person.HandicapHearing.Add(false, random2);
+            GenerateRandomDistribution(out random1, out random2);
+            this.Person.HandicapMobility.Add(true, random1);
+            this.Person.HandicapMobility.Add(false, random2);
+            GenerateRandomDistribution(out random1, out random2);
+            this.Person.HandicapSpeaking.Add(true, random1);
+            this.Person.HandicapSpeaking.Add(false, random2);
+            GenerateRandomDistribution(out random1, out random2);
+            this.Person.HandicapVision.Add(true, random1);
+            this.Person.HandicapVision.Add(false, random2);
+
+
+        }
+
+        
+
+        /// <summary>
+        ///  Gute Zufallszahlen erzeugen
+        ///  Quelle: http://www.hofmann-robert.info/?p=360
+        /// </summary>
+        protected static int GenerateRandomNumber(int min, int max)
+        {
+            RNGCryptoServiceProvider c = new RNGCryptoServiceProvider();
+            // Integer benötigt 4 Byte
+            byte[] randomNumber = new byte[4];
+            // Array mit zufälligen Bytes befüllen
+            c.GetBytes(randomNumber);
+            // Byte-Array in einen Integer umwandeln
+            int result = Math.Abs(BitConverter.ToInt32(randomNumber, 0));
+            // Begrenzung durch Modulo Rechnung
+            return result % max + min;
+        }
+
+        /// <summary>
+        /// Erzeuge eine zufällige Wahrscheinlichkeitsverteilung auf zwei Werte
+        /// </summary>
+        /// <param name="randomDist1">Erster Zufälliger wert zwischen 0 und 1.0, Summe aus beiden Werten ergibt 1.0</param>
+        /// <param name="randomDist2">Zweiter Zufälliger wert zwischen 0 und 1.0, Summe aus beiden Werten ergibt 1.0</param>
+        protected static void GenerateRandomDistribution(out float randomDist1, out float randomDist2)
+        {
+            // TODO: wenn Funktion schnell hintereinander aufgerufen wird erzeugt NextDouble() selben Zufallswert (TimeStamp!)
+            Random rnd = new Random();
+            randomDist1 = (float)rnd.NextDouble();
+            randomDist2 = (float)1.0 - randomDist1;
+        }
+
+    }
+}
